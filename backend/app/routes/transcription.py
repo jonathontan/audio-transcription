b573ecbab56c620d7ce255d9f1711d1054950b30
m3-jonathontan <jonathon.tan@maventree.sg>
2026-03-17 13:52:50 +0800

    feat: add transcription router get and post

--- backend/app/routes/transcription.py
+++ backend/app/routes/transcription.py
@@ -1,24 +1,84 @@
 import os
-import shutil
+import aiofiles
 
 from fastapi import APIRouter, UploadFile, File, Depends, HTTPException
-from schemas import TranscriptionResponse
-from database import SessionLocal
-from crud import create_transcription, get_all_transcriptions
-from utils import generate_unique_filename
-from whisper_service import transcribe_audio
+from typing import Annotated
+from sqlalchemy.orm import Session
+
+from app.schemas import TranscriptionResponse
+from app.database import SessionLocal
+from app.crud import create_transcription, get_all_transcriptions
+from app.utils import (
+    generate_unique_filename,
+    is_valid_file_type,
+    ALLOWED_EXTENSIONS,
+    MAX_FILE_SIZE,
+)
+from app.whisper_service import transcribe_audio
 
 router = APIRouter()
 
 os.makedirs("uploads", exist_ok=True)
 
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
-    finally: 
+    finally:
         db.close()
 
-@router.get("/transcriptions", tags=["transcriptions"], response_model=list[TranscriptionResponse])
-def get_transcriptions(db: Session = Depends(get_db)):
+
+@router.get(
+    "/transcriptions",
+    tags=["Transcriptions"],
+    response_model=list[TranscriptionResponse],
+)
+def get_transcriptions(db: Annotated[Session, Depends(get_db)]):
     return get_all_transcriptions(db)
+
+
+@router.post(
+    "/generate",
+    tags=["Transcriptions"],
+    response_model=TranscriptionResponse,
+    responses={
+        400: {"description": "Invalid request or unsupported file type"},
+        500: {"description": "Fail to generate"},
+    },
+)
+async def generate_transcriptions(
+    file: Annotated[UploadFile, File(...)], db: Annotated[Session, Depends(get_db)]
+):
+    if not file.filename:
+        raise HTTPException(status_code=400, detail="File name is missing")
+    if not is_valid_file_type(file.filename, file.content_type):
+        raise HTTPException(
+            status_code=400,
+            detail=f"Unsupported file type. Accept: {', '.join(ALLOWED_EXTENSIONS)}",
+        )
+    new_filename = generate_unique_filename("uploads", file.filename)
+    file_path = os.path.join("uploads", new_filename)
+
+    try:
+        async with aiofiles.open(file_path, "wb") as buffer:
+            while chunk := await file.read(1024 * 1024):
+                if len(chunk) > MAX_FILE_SIZE:
+                    raise HTTPException(
+                        status_code=400,
+                        detail="File exceeds limit. Maximum size is 1MB",
+                    )
+                await buffer.write(chunk)
+
+        audio_transcript = transcribe_audio(file_path)
+        data = create_transcription(
+            db=db,
+            filename=new_filename,
+            filepath=file_path,
+            transcript=audio_transcript,
+        )
+
+        return data
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
