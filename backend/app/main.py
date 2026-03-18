aa7bb1adb7613a1829525b54d22c8b589c58a858
m3-jonathontan <jonathon.tan@maventree.sg>
2026-03-18 17:48:53 +0800

    feat: add cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import Base, engine
 from app.routes.health import router as health_router
 from app.routes.transcription import router as transcription_router
@@ -10,6 +11,14 @@ app = FastAPI(
     title="Audio Transcription API",
     version="1.0.0",
     docs_url="/swagger",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 app.include_router(health_router)
