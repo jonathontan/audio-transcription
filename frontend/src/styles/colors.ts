0d10b37439af3469cd6df6f72bb51f5590fb9e4f
m3-jonathontan <jonathon.tan@maventree.sg>
2026-03-19 13:03:40 +0800

    style: update index css and add colors scheme

---
+++ frontend/src/styles/colors.ts
@@ -0,0 +1,13 @@
+const white = "#ffffff";
+const black = "#000000";
+const border = "#e5e4e7";
+const secondarybg = "#f5f5f5";
+const accent = "#5c068c";
+
+export default {
+  white,
+  black,
+  border,
+  secondarybg,
+  accent,
+};
