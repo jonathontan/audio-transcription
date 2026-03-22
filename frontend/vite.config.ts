b371c91ae9bccd869b7c3687b3f1d7b0779d60a5
m3-jonathontan <jonathon.tan@maventree.sg>
2026-03-22 17:08:32 +0800

    chore: add test environment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,4 +8,9 @@ export default defineConfig({
     port: 3000,
   },
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+  },
+});
